22c5204df2598b12c1450eda2a55fd52c3f47ad5
Matt Derasoirs <matt@planwork.us>
2014-09-01 14:30:48 -0400

    :video_camera:
    Replacing hack code with oo

--- backend/file.js
+++ backend/file.js
@@ -1,1 +1,60 @@
-alert('file read');
+/**
+ * Canvas Reader
+ * @param {[type]} id   [description]
+ * @param {[type]} opts [description]
+ */
+function CanvasReader (el, opts) {
+  opts = opts || {};
+  this.fps = opts.fps || 33;
+  this.socket_url = opts.socket_url || window.location.origin;
+  this.socket     = io(this.socket_url);
+  this.canvas_id  = typeof el === 'string' ? el : '';
+  this.canvas     = document.getElementById(el);
+  this.is_running = false;
+}
+
+/**
+ * Loop
+ * @type {[type]}
+ */
+CanvasReader.prototype.loop = functino () {
+  if (this.is_running) {
+    this.send.call(this);
+    setTimeout(this.loop, 1.0/this.fps);
+  }
+}
+
+/**
+ * [start description]
+ * @return {[type]} [description]
+ */
+CanvasReader.prototype.start = function () {
+  this.is_running = true;
+  this.socket.emit('start', this);
+  this.loop();
+};
+
+/**
+ * Read DataURL from Canvas Element
+ * @return {[type]} [description]
+ */
+CanvasReader.prototype.read = function () {
+  return this.canvas.toDataURL('image/jpeg');
+};
+
+/**
+ * [send description]
+ * @return {[type]} [description]
+ */
+CanvasReader.prototype.send = function () {
+  this.socket.emit('image', this.read());
+}
+
+/**
+ * Close Stream and 
+ * @return {[type]} [description]
+ */
+CanvasReader.prototype.end = function () {
+  this.socket.emit('end', true);
+  this.socket.disconnect();
+}
